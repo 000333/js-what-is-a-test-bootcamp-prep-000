f618d63d886a2802663c66d3c3d22708aa37ec7a
Marissa B <m1bertulfo@gmail.com>
2018-01-14 04:17:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe";
+var name_ = "Joe";
 var height = 74;
-var message = `${name} is ${height} inches tall`;
+var message = `${name_} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = {name_, height, message};
